347be07d8f49f077bdb2e2b80eb71d92e48ccac5
Code-Mole <moleasangabia23@gmail.com>
2025-10-15 09:50:42 +0000

    giving cors access

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ import tokenRoute from "./router/tokenRoute.js";
 const server = express();
 dotenv.config();
 connectDB();
-server.use(cors());
+server.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? ["https://qrcode-system-frontend.onrender.com"]
+        : ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 server.use(express.json());
 
 // Routes
